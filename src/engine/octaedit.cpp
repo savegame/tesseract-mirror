f726a682f46fd1c4a8adf0e475bf71264bd89337
eihrul <eihrul@b412e182-2ed4-4078-95c4-27dc59f57fa9>
2020-01-21 02:24:35 +0000

    getvcolor command

--- src/engine/octaedit.cpp
+++ src/engine/octaedit.cpp
@@ -2306,6 +2306,12 @@ void vcolor(float *r, float *g, float *b)
     mpeditvslot(usevdelta, ds, allfaces, sel, true);
 }
 COMMAND(vcolor, "fff");
+ICOMMAND(getvcolor, "i", (int *tex),
+{
+    VSlot &vslot = lookupvslot(*tex, false);
+    defformatstring(str, "%s %s %s", floatstr(vslot.colorscale.r), floatstr(vslot.colorscale.g), floatstr(vslot.colorscale.b));
+    result(str);
+});
 
 void vrefract(float *k, float *r, float *g, float *b)
 {
