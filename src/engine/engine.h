fe59a8575b51894e5fa9addc0e413112d5de7f2c
eihrul <eihrul@b412e182-2ed4-4078-95c4-27dc59f57fa9>
2020-04-26 04:47:47 +0000

    remove old shadowray code

--- src/engine/engine.h
+++ src/engine/engine.h
@@ -639,7 +639,6 @@ extern void modifyorient(float yaw, float pitch);
 extern void mousemove(int dx, int dy);
 extern bool overlapsdynent(const vec &o, float radius);
 extern void rotatebb(vec &center, vec &radius, int yaw, int pitch, int roll = 0);
-extern float shadowray(const vec &o, const vec &ray, float radius, int mode, extentity *t = NULL);
 
 // world
 
