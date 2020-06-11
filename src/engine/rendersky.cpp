e329a7194f7474fc9471bc9ec2acd5767021805a
eihrul <eihrul@b412e182-2ed4-4078-95c4-27dc59f57fa9>
2020-06-11 14:18:27 +0000

    brighten sundisk now that tonemapping is saner

--- src/engine/rendersky.cpp
+++ src/engine/rendersky.cpp
@@ -436,7 +436,7 @@ static void drawatmosphere()
 
     // scale extinguished sunlight in ratio to extinction at zenith, then clamp to force saturation
     vec zenithextinction = vec(betarm).mul(-(sundist - (atmoratio - 1))).exp();
-    vec diskcolor = (!atmosundisk.iszero() ? atmosundisk.tocolor() : suncolor).mul(ldrscale).pow(hdrgamma).mul(zenithextinction).mul(atmosundiskbright);
+    vec diskcolor = (!atmosundisk.iszero() ? atmosundisk.tocolor() : suncolor).mul(ldrscale).pow(hdrgamma).mul(zenithextinction).mul(atmosundiskbright * 2);
     LOCALPARAM(sunlight, vec4(diskcolor, atmoalpha));
     LOCALPARAM(sundir, sunlightdir);
 
